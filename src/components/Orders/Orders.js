0d6f7f9ad5f173455ac762be306506afd0333cd2
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-04-30 01:05:59 +0600

    Pagination implemented

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -3,19 +3,17 @@ import { Link } from 'react-router-dom';
 import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import useCart from '../hooks/useCart';
-import useProducts from '../hooks/useProducts';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css'
 
 const Orders = () => {
-    const [products, setProducts] = useProducts()
 
-    const [cart, setCart] = useCart(products)
+    const [cart, setCart] = useCart()
 
     const handleRemoveProduct = (product) => {
-        const rest = cart.filter(pd => pd.id !== product.id)
+        const rest = cart.filter(pd => pd._id !== product._id)
         setCart(rest)
-        removeFromDb(product.id)
+        removeFromDb(product._id)
     }
 
     return (
@@ -23,7 +21,7 @@ const Orders = () => {
             <div className='review-items-container'>
                 {
                     cart.map(product => <ReviewItem
-                        key={product.id}
+                        key={product._id}
                         product={product}
                         handleRemoveProduct={handleRemoveProduct}
                     ></ReviewItem>)
